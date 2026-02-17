209216ca6cc22416c54bab6d1332d31a9be25696
SarawutChan <sarawut.chantasri@gmail.com>
2026-02-17 10:14:16 +0700

    Add def

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 # my first script
 print("Hello World")
 print("I am using github on posit.cloud")
+
+def hello():
+  name = input("What is your name: ")
+  print(f"Hello {name}!")
+  
+# test function
+hello()
